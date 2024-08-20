e77bef928b0cd4562eea5579348f2692b118e1be
luizsoaress <129185289+luizsoaress@users.noreply.github.com>
2024-08-20 08:36:44 -0300

    akakakakaka

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-const teste =document.querySelector(".caixa-principal");
+const elemento = document.querySelector(".teste");
+
+const titulo = document.querySelector(".teste2");
+
+titulo. textcontent = “olá mundo";
+
+
+Let contador = 1;
+
+Let contador =  [1,2,5,8,9];
